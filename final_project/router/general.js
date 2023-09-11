97b582e3fce4ffc140542a61dc76d79fc9dbba02
VasiliyMac <cartario@yandex.ru>
2023-09-11 19:26:38 +0400

    task-2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,15 +11,13 @@ public_users.post('/register', (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  //Write your code here
   return res.status(200).json({ books });
-  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn]);
 });
 
 // Get book details based on author
